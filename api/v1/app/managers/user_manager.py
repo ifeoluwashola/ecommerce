ba37489889d108a4fa517723a985d02adaac6327
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2024-12-12 13:06:30 +0100

    add requirements.txt

--- api/v1/app/managers/user_manager.py
+++ api/v1/app/managers/user_manager.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
+from ....supabase.supabase_client import supabase
+
 
 class UserManager:
-    pass
+    """This class is going to handle all user related logic"""
+    @staticmethod
+    async def get_all_user():
+        """
+        This method queries the database through supabase to get all users from the database:
+        :returns - list of users
+        """
+        result = supabase.table("users").select("*").execute()
+        return result
---
+++ api/v1/app/resources/auth.py
@@ -0,0 +1,2 @@
+class AuthManager:
+    pass
